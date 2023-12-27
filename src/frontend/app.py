17347884c6d113b16819280c6bf8a6986892ddd8
Rishi Gundakaram <rishi.gundakaram@gmail.com>
2023-12-26 19:43:01 -0500

    updated frontend

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -32,48 +32,90 @@ def color_red_gradient(val):
     return f"color: rgb({red}, 0, 0)"
 
 
+import re
+import requests
+
+
+def extract_date_from_filename(filename):
+    pattern = r"(\d{4}_\d{2}_\d{2})"
+    match = re.search(pattern, filename)
+    return match.group(1) if match else None
+
+
+def get_files_and_dates(repo, path):
+    url = f"https://api.github.com/repos/{repo}/contents/{path}"
+    response = requests.get(url)
+    files_dates = {}
+
+    if response.status_code == 200:
+        files = response.json()
+        for file in files:
+            if file["type"] == "file":
+                date = extract_date_from_filename(file["name"])
+                if date:
+                    files_dates[file["name"]] = date
+    else:
+        print("Failed to retrieve repository data")
+
+    return files_dates
+
+
 # Apply the styling
+def convert_date(date):
+    date = date.replace("_", "/")
+    return date[5:] + "/" + date[:4]
 
 
 def main():
     st.set_page_config(
         page_title="MLB Forecasting", page_icon=":baseball", layout="wide"
     )
-    st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Choose a page", ["Predictions", "Model Info"])
+    repo = "rishigundakaram/baseball-MCS"
+    path = "data/final"
+    files_dates = get_files_and_dates(repo, path)
+    dates = list(files_dates.values())
+    dates.sort(reverse=True)
+    date = st.sidebar.selectbox(
+        "Choose a date", options=dates, format_func=convert_date
+    )
+    filename = [k for k, v in files_dates.items() if v == date][0]
+    probabilities = pd.read_csv(
+        f"https://raw.githubusercontent.com/{repo}/main/{path}/{filename}"
+    )
 
-    if page == "Predictions":
-        custom_style()
-        probabilities = pd.read_csv(
-            "https://raw.githubusercontent.com/rishigundakaram/baseball-MCS/main/data/final/probabilities.csv"
-        )
-        # probabilities = pd.read_csv(
-        #     "/home/projects/baseball-MCS/data/final/probabilities.csv"
-        # )
-        probabilities = probabilities.drop(probabilities.columns[0], axis=1)
-        # probabilities = probabilities.style.applymap(
-        #     color_red_gradient,
-        #     subset=["Make Playoffs %", "Win Division %", "Win World Series %"],
-        # )
+    custom_style()
+    # probabilities = pd.read_csv(
+    #     "https://raw.githubusercontent.com/rishigundakaram/baseball-MCS/main/data/final/2023_02_01probabilities.csv"
+    # )
+    # probabilities = pd.read_csv(
+    #     "/home/projects/baseball-MCS/data/final/probabilities.csv"
+    # )
+    probabilities = probabilities.drop(probabilities.columns[0], axis=1)
+    # probabilities = probabilities.style.applymap(
+    #     color_red_gradient,
+    #     subset=["Make Playoffs %", "Win Division %", "Win World Series %"],
+    # )
 
-        print(probabilities)
-        st.title("2024 MLB Season Predictions")
+    st.title("2024 MLB Season Predictions")
 
-        st.dataframe(
-            probabilities,
-            hide_index=True,
-            column_config={
-                "logo": st.column_config.ImageColumn(label=""),
-                "Elo": st.column_config.LineChartColumn(label="Elo"),
-            },
-        )
-        st.divider()
-        st.subheader("Blog Posts")
-        medium_blog_url = "https://medium.com/@rishi.gundakaram/baseball-forecasting-using-monte-carlo-simulation-f789ba0abee2"
-        st.markdown(f"[How Are These Probabilities Calculated?]({medium_blog_url})")
-    elif page == "Model Info":
-        st.title("About the Model")
-        st.write("A Blog post about the model")
+    st.dataframe(
+        probabilities,
+        hide_index=True,
+        column_config={
+            "logo": st.column_config.ImageColumn(label=""),
+            "Elo": st.column_config.LineChartColumn(label="Elo"),
+        },
+    )
+    # italicize and right align the date of the predictions
+    st.markdown(
+        f"<p style='text-align: right; font-style: italic;'>Predictions from {convert_date(date)}</p>",
+        unsafe_allow_html=True,
+    )
+    # st.write(f"Predictions From {convert_date(date)}")
+    st.divider()
+    st.subheader("Blog Posts")
+    medium_blog_url = "https://medium.com/@rishi.gundakaram/baseball-forecasting-using-monte-carlo-simulation-f789ba0abee2"
+    st.markdown(f"[How Are These Probabilities Calculated?]({medium_blog_url})")
 
 
 if __name__ == "__main__":
